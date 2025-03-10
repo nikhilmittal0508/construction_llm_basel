15595472ff4a40e36b897473e31bbe23556cfdd5
root <root@ip-172-31-26-100.eu-north-1.compute.internal>
2025-03-10 02:35:05 +0000

    first commit

---
+++ main_old.py
@@ -0,0 +1,441 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api
+from utils import *
+from zipfile import ZipFile
+# import transformers
+# from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline
+
+from langchain.retrievers.self_query.base import SelfQueryRetriever
+from langchain.chains.query_constructor.base import AttributeInfo
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter, TextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Qdrant
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import UnstructuredFileLoader, TextLoader, JSONLoader
+from langchain.llms import LlamaCpp, GPT4All
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate 
+from langchain.prompts import SystemMessagePromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate
+from qdrant_client.http import models
+from qdrant_client import QdrantClient
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.callbacks.manager import CallbackManager
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from sentence_transformers import CrossEncoder
+import json
+
+import openai
+from langchain.llms import OpenAI
+import shutil
+
+
+config_file_path = 'params.json'
+
+with open(config_file_path, 'r') as config_file:
+    config_data = json.load(config_file)
+    config = config_data.get('config', {})
+    
+    
+hal_model = CrossEncoder('vectara/hallucination_evaluation_model')
+
+# Initiate the memory
+memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        return_messages=True,
+        output_key='answer'
+    )
+
+
+
+qdrant_client = QdrantClient(
+    url=config['url'], 
+    api_key=config['qdrant_api_key'],
+)
+
+app = Flask(__name__)
+api = Api(app)
+
+
+
+@app.route('/api/create_collection', methods=['POST'])
+def create_new_collection():
+    inputs = request.json
+    collection_name = inputs['collection_name']
+    model_type = inputs['model_type']
+    
+    collection_name = collection_name + "_" + model_type
+    if model_type == "openai":
+        vector_size =  1536
+    else:
+        vector_size =  384
+    
+    response = create_one_collection(collection_name, vector_size, qdrant_client)
+    if response:
+        source_folder = config['source_docs']
+        folder_path = os.path.join(source_folder, collection_name)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        return jsonify(response)    
+    
+    return jsonify(response)
+
+
+@app.route('/api/get_all_collections', methods=['POST'])
+def get_all_collection():
+    inputs = request.json
+    model_type = inputs['model_type']
+    
+    collection_list = []
+    all_collections =  qdrant_client.get_collections().collections
+    for i in all_collections:
+        if model_type in i.name:
+            collection_list.append(i.name)
+    return jsonify(collection_list)
+
+
+@app.route('/api/get_all_folders', methods=['POST'])
+def getting_all_the_folder_names_from_collection_folder():
+    inputs = request.json
+    collection_name = inputs['collection_name']
+    
+    source_folder = config['source_docs']
+    collection_folder_path = os.path.join(source_folder, collection_name)
+    folders = [folder for folder in os.listdir(collection_folder_path) if os.path.isdir(os.path.join(collection_folder_path, folder))]
+    return jsonify(folders)
+
+
+@app.route('/api/get_all_filenames', methods=['POST'])
+def getting_all_the_file_names_from_collection_folder():
+    inputs = request.json
+    collection_name = inputs['collection_name']
+    
+    source_folder = config['source_docs']
+    collection_folder_path = os.path.join(source_folder, collection_name)
+    files = [item for item in os.listdir(collection_folder_path) if os.path.isfile(os.path.join(collection_folder_path, item))]
+    return jsonify(files)
+
+
+@app.route('/api/delete_collection', methods=['POST'])
+def delete_collection():
+    inputs = request.json
+    collection_name = inputs['collection_name']
+   
+    #deleting the collection in qdrant
+    deletion_status = qdrant_client.delete_collection(collection_name=collection_name)
+    
+    #deleting the collection folder from source folder
+    # Check if the file exists before attempting to delete
+    source_folder = config['source_docs']
+    collection_folder_path = os.path.join(source_folder, collection_name)
+    
+    if os.path.exists(collection_folder_path):
+        # If it exists, delete the folder
+        shutil.rmtree(collection_folder_path)
+        print(f"Folder '{collection_folder_path}' has been deleted.")
+    else:
+        print(f"Folder '{collection_folder_path}' does not exist.")
+        
+    return jsonify(deletion_status)
+
+
+
+@app.route('/api/delete_file', methods=['POST'])
+def delete_single_file():
+    inputs = request.json
+    collection_name = inputs['collection_name']
+    filename = inputs['filename']
+    
+    resp_qdrant = delete_file_qdrant(filename, collection_name, qdrant_client)
+    if resp_qdrant:
+        resp_source_file = delete_file_from_source_collection_folder(filename, collection_name, config['source_docs'])
+        return jsonify(resp_source_file)
+    
+    return jsonify(resp_qdrant) # boolean return
+
+
+@app.route('/api/delete_folder', methods=['POST'])
+def delete_folder():
+    inputs = request.json
+    collection_name = inputs['collection_name']
+    folder_name = inputs['folder_name']
+    
+    resp_qdrant = delete_folder_qdrant(folder_name, collection_name, qdrant_client)
+    if resp_qdrant:
+        resp_source_folder = delete_folder_from_source_collection_folder(folder_name, collection_name, config['source_docs'])
+        return jsonify(resp_source_folder)
+    
+    return jsonify(resp_qdrant) # boolean return
+
+
+@app.route('/api/file_upload_qdrant', methods=['POST'])
+def file_ingestion_qdrant():
+    
+    collection_name = request.form["collection_name"]
+    model_type = request.form["model_type"]
+    
+    # Check if the POST request has the file part
+    if 'file' not in request.files:
+        return "No file part"
+
+    uploaded_file = request.files['file']
+    print("type: ",type(uploaded_file))
+
+    # Check if a file is selected
+    if uploaded_file.filename == '':
+        return "No selected file"
+    
+    # Check if the file type is allowed
+    if uploaded_file.filename.lower().endswith((".txt", ".pdf", ".doc", ".xlsx", ".pptx", ".csv", ".html", ".json")):
+        
+        already_ingested_files = check_qdrant_ingested_files(collection_name, qdrant_client)
+        
+        # Check if the file is already ingested
+        if uploaded_file.filename in already_ingested_files:
+            return f"File '{uploaded_file.filename}' is already ingested."
+        
+        source_folder = config['source_docs']
+        folder_path = os.path.join(source_folder, collection_name)
+        file_path = os.path.join(folder_path,  uploaded_file.filename)
+        
+        # Save the file in the source folder
+        uploaded_file.save(file_path)
+        
+        if uploaded_file.filename.lower().endswith((".json")):
+            loader = JSONLoader(file_path, jq_schema='.content')
+            documents = loader.load()
+            
+        elif uploaded_file.filename.lower().endswith((".csv")):
+            loader = CSVLoader(file_path)
+            documents = loader.load()
+        
+        else:
+            loader = UnstructuredFileLoader(file_path)
+            documents = loader.load()
+
+        #Split documents into chunks
+        texts =  texts_splitter(documents)
+
+        # Ingestion
+        response = ingesting_file_qdrant_db(collection_name, texts, model_type, config["url"], config["qdrant_api_key"], config['embedding_model'], config['cache_folder'], config['open_api_key'])
+        
+        if response:
+            return "Ingesting Done"
+        else:
+            return "Ingestion Failed"
+    else:
+        return "Error: Unsupported file type"
+    
+    
+    
+@app.route('/api/folder_upload_qdrant', methods=['POST'])
+def ingest_folder():
+    try:
+        collection_name = request.form["collection_name"]
+        model_type = request.form["model_type"]
+        selected_folder = request.form["selected_folder"]
+
+        # Specify the destination folder path
+        source_folder = config['source_docs']
+        destination_folder = os.path.join(source_folder, collection_name)
+
+        print("destination_folder: ",destination_folder)
+         # Construct the destination folder path including the selected folder
+        destination_with_selected = os.path.join(destination_folder, os.path.basename(selected_folder))
+
+        print("destination_with_selected: ",destination_with_selected)
+
+        # Copy the entire folder and its contents to the destination
+        # Check if the destination folder exists
+        if os.path.exists(destination_with_selected):
+            print(f"Destination folder '{os.path.basename(selected_folder)}' already exists.")
+
+            # You can choose to overwrite the existing folder or take other actions
+            # For example, you might want to prompt the user for confirmation
+
+#             user_response = input("Do you want to overwrite the existing folder? (yes/no): ").lower()
+
+#             if user_response == 'yes':
+#                 # If the user confirms, copy the entire folder and its contents to the destination
+#                 shutil.copytree(selected_folder, destination_folder)
+#                 print(f"Folder '{selected_folder}' has been copied to '{destination_folder}'.")
+#             else:
+#                 print("Operation aborted.")
+        else:
+            # If the destination folder doesn't exist, copy the entire folder and its contents to the destination
+            shutil.copytree(selected_folder, destination_with_selected)
+            print(f"Folder '{os.path.basename(selected_folder)}' has been copied to '{destination_folder}'.")
+            
+
+        folder_path = destination_with_selected
+        print(folder_path)
+        file_list = [filename for filename in os.listdir(folder_path) if filename.lower().endswith((".txt", ".pdf", ".doc", ".xlsx", ".pptx", ".csv", ".html", ".json"))]
+        print("to_be_ingested:",file_list)
+
+        if len(file_list) > 0:
+            for i, filename in tqdm.tqdm(enumerate(file_list)):
+                file_path = os.path.join(folder_path, filename)
+                
+                if file_path.endswith((".json")):
+                    loader = JSONLoader(file_path, jq_schema='.content')
+                    documents = loader.load()
+
+                elif file_path.endswith((".csv")):
+                    loader = CSVLoader(file_path)
+                    documents = loader.load()
+
+                else:
+                    loader = UnstructuredFileLoader(file_path)
+                    documents = loader.load()
+                
+                #Split documents into chunks
+                texts =  texts_splitter(documents)
+                
+                # Ingestion
+                response = ingesting_file_qdrant_db(collection_name, texts, model_type, config["url"], config["qdrant_api_key"], config['embedding_model'], config['cache_folder'], config['open_api_key'])
+
+        return jsonify({"output":"Ingestion Complete"})
+    
+    except Exception as e:  # Catch any general exception
+        return jsonify({"output": "Ingestion Incomplete", "error": str(e)})
+    
+    
+    
+@app.route('/api/get_answer', methods=['POST'])
+def get_answer():
+    inputs = request.json
+    query = inputs['query']
+    no_of_source = inputs['no_of_source']
+    qa_prompt = inputs['user_prompt']
+    model_type = inputs["model_type"]
+    collection_name = inputs["collection_name"]
+    
+    print(inputs)
+    
+    if not qa_prompt:
+        # Use a generic QA prompt if user prompt is empty
+        qa_prompt = """Answer the question based on the context below. If the
+                       question cannot be answered using the information provided answer
+                       with 'I don't know' """
+        
+    general_system_template = r"""
+        ----
+        {context}
+        ----
+        """
+    general_system_template = qa_prompt + "\n" + general_system_template
+    general_user_template = "Question:```{question}```"
+    messages = [
+                SystemMessagePromptTemplate.from_template(general_system_template),
+                HumanMessagePromptTemplate.from_template(general_user_template)
+    ]
+    prompt = ChatPromptTemplate.from_messages(messages)
+
+    # print(prompt)
+    
+    if model_type == "openai":
+        embedding_function = OpenAIEmbeddings(openai_api_key=config["open_api_key"])
+    elif model_type == "mistral":
+        embedding_function = HuggingFaceEmbeddings(model_name=config['embedding_model'], cache_folder=config['cache_folder'])
+    
+    # qdrant_client = QdrantClient(url=url, api_key=api_key)
+    qdrant_db = Qdrant(qdrant_client, embeddings=embedding_function, collection_name=collection_name)
+    retriever = qdrant_db.as_retriever(search_type="mmr", search_kwargs={"k": no_of_source})
+    
+    
+    if model_type == "openai":
+        # Create QA chain
+        llm = OpenAI(openai_api_key=config["open_api_key"], model="gpt-3.5-turbo-instruct", max_tokens=2000)
+        
+    elif model_type == "mistral":
+        
+        llm = LlamaCpp(
+            streaming = True,
+            model_path=config['model_path'],
+            temperature=0.75,
+            top_p=1,
+            verbose=True,
+            n_ctx=4096,
+            n_gpu_layers=-1,
+            n_batch=1000,
+            max_tokens = 2000,
+            callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+        )
+        
+    # qa_chain = RetrievalQA.from_chain_type(llm=llm, chain_type='refine', retriever=retriever, return_source_documents=True)
+    qa_chain = ConversationalRetrievalChain.from_llm(
+            llm=llm,
+            retriever=retriever,
+            memory=memory,
+            return_source_documents=True,
+            combine_docs_chain_kwargs={"prompt": prompt},
+        )
+
+    # Run QA
+    # result = qa_chain({"query": query})
+    result = qa_chain({"question": query})
+
+
+    # hal_score = hal_model.predict([result['question'],result['answer']])
+
+
+    new_result = {}
+    new_result["question"] = result["question"]
+    new_result["answer"] = result["answer"]
+
+    result_list = []
+    for document in result["source_documents"]:
+        result_dict = {
+            'page_content': document.page_content,
+            'metadata': document.metadata
+        }
+        result_list.append(result_dict)
+
+    # print(result_list)
+    new_result["source_documents_list"] = result_list
+    
+    # if hal_score > 0.3:
+    #     new_result["is_hallucination"] = False
+    # else:
+    #     new_result["is_hallucination"] = True
+        
+
+    
+    # result = qa.run(query)
+    return jsonify(new_result)
+
+
+@app.route('/api/get_relevant_docs', methods=['POST'])
+def get_relevant_documents():
+    inputs = request.json
+    query = inputs['query']
+    no_of_source = inputs['no_of_source']
+    model_type = inputs["model_type"]
+    collection_name = inputs["collection_name"]
+    
+    # print(inputs)
+    
+    if model_type == "openai":
+        embedding_function = OpenAIEmbeddings(openai_api_key=config["open_api_key"])
+    elif model_type == "mistral":
+        embedding_function = HuggingFaceEmbeddings(model_name=config['embedding_model'], cache_folder=config['cache_folder'])
+    
+    # qdrant_client = QdrantClient(url=url, api_key=api_key)
+    qdrant_db = Qdrant(qdrant_client, embeddings=embedding_function, collection_name=collection_name)
+    retriever = qdrant_db.as_retriever(search_type="mmr", search_kwargs={"k": no_of_source})
+    docs = retriever.get_relevant_documents(query)
+    # Sources of the documents
+    source = {}
+    for i in range(len(docs)):
+        source[str(i+1)+"_Page Content"] = docs[i].page_content
+        source[str(i+1)+"_Doc Name"] = docs[i].metadata
+        
+    return jsonify(source)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=False, port=5000)
---
+++ utils.py
@@ -0,0 +1,258 @@
+import os
+import tqdm
+from dotenv import load_dotenv,find_dotenv
+from pathlib import Path
+
+from flask import Flask, jsonify, request
+from flask_restful import Api
+
+# import transformers
+# from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline
+
+from langchain.retrievers.self_query.base import SelfQueryRetriever
+from langchain.chains.query_constructor.base import AttributeInfo
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter, TextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Qdrant
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import UnstructuredFileLoader, TextLoader, JSONLoader
+from langchain.llms import LlamaCpp, GPT4All
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate 
+from langchain.prompts import SystemMessagePromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate
+from qdrant_client.http import models
+from qdrant_client import QdrantClient
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.callbacks.manager import CallbackManager
+from langchain_community.document_loaders.csv_loader import CSVLoader
+#from sentence_transformers import CrossEncoder
+
+import openai
+from langchain.llms import OpenAI
+import shutil
+
+
+def create_one_collection(name_of_the_collection, dimension, qdrant_client):
+    # print(name_of_the_collection, dimension, qdrant_client)
+    try:
+        qdrant_client.create_collection(
+            collection_name=name_of_the_collection,
+            vectors_config=models.VectorParams(size=dimension, distance=models.Distance.COSINE),
+        )
+        return True
+    except Exception as e:
+        # print(e)
+        return False
+    
+
+def delete_file_qdrant(filename_to_del, collection, qdrant_client):
+    try:
+        source_folder = "source_documents"
+        collection_folder_path = os.path.join(source_folder, collection)
+        # print(collection_folder_path)
+        file_name = os.path.join(collection_folder_path, filename_to_del)
+        qdrant_client.delete(
+            collection_name=collection,
+            points_selector=models.FilterSelector(
+                filter=models.Filter(
+                    must=[
+                        models.FieldCondition(
+                            key="metadata.source",
+                            match=models.MatchAny(any=[file_name]),
+                        ),
+                    ],
+                )
+            ),
+        )
+        return True
+
+    except Exception as e:
+        print(e)
+        return False
+    
+    
+# def delete_file_qdrant(filename_to_del, collection, qdrant_client):
+#     a = qdrant_client.scroll(
+#     collection_name=collection,
+#     scroll_filter=models.Filter(),
+#     limit=100000000000
+#     )
+    
+#     unique_filenames = {}
+#     for entry in a[0]:
+#         filename = os.path.basename(entry.payload["metadata"]['source'])
+#         entry_id = entry.id
+
+#         if filename in unique_filenames:
+#             unique_filenames[filename].append(entry_id)
+#         else:
+#             unique_filenames[filename] = [entry_id]
+            
+#     # print(unique_filenames)
+    
+#     try:
+#         resp =  qdrant_client.delete(
+#             collection_name=collection,
+#             points_selector=models.PointIdsList(
+#                 points=unique_filenames[filename_to_del],
+#             ),
+#         )
+#         return True
+    
+#     except Exception as e:
+#         print(e)
+#         return False
+
+
+
+def delete_file_from_source_collection_folder(filename_to_del, collection, source_docs):
+    # Specify the file path (relative or absolute)
+    source_folder = source_docs
+    folder_path = os.path.join(source_folder,collection)
+    file_path = os.path.join(folder_path, filename_to_del)
+    
+    # Check if the file exists before attempting to delete
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        print(f"File '{file_path}' has been deleted.")
+        return True
+    else:
+        print(f"File '{file_path}' not found.")
+        return False
+
+
+def getting_all_the_file_names_from_one_collection_folder(collection_name, folder_name_to_del):
+    source_folder = "source_documents"
+    collection_folder_path = os.path.join(source_folder, collection_name)
+    # print(collection_folder_path)
+    folder_path = os.path.join(collection_folder_path, folder_name_to_del)
+    # print(folder_path)
+
+    files = [os.path.join(folder_path,item) for item in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, item))]
+    return files
+
+
+def delete_folder_qdrant(folder_name_to_del, collection, qdrant_client):
+    try:
+        file_list = getting_all_the_file_names_from_one_collection_folder(collection, folder_name_to_del)
+        qdrant_client.delete(
+            collection_name=collection,
+            points_selector=models.FilterSelector(
+                filter=models.Filter(
+                    must=[
+                        models.FieldCondition(
+                            key="metadata.source",
+                            match=models.MatchAny(any=file_list),
+                        ),
+                    ],
+                )
+            ),
+        )
+        return True
+
+    except Exception as e:
+        print(e)
+        return False
+
+# def delete_folder_qdrant(folder_name_to_del, collection, qdrant_client):
+#     a = qdrant_client.scroll(
+#     collection_name=collection,
+#     scroll_filter=models.Filter(),
+#     limit=100000000000
+#     )
+    
+#     unique_filenames = {}
+#     unique_filenames[folder_name_to_del] = []
+#     for entry in a[0]:
+#         filename = entry.payload["metadata"]['source']
+#         entry_id = entry.id
+        
+#         if folder_name_to_del in filename:
+#             unique_filenames[folder_name_to_del].append(entry_id)
+            
+#     # print( unique_filenames )
+#     try:
+#         resp =  qdrant_client.delete(
+#             collection_name=collection,
+#             points_selector=models.PointIdsList(
+#                 points=unique_filenames[folder_name_to_del],
+#             ),
+#         )
+#         return True
+    
+#     except Exception as e:
+#         print(e)
+#         return False
+    
+    
+def delete_folder_from_source_collection_folder(folder_name_to_del, collection, source_docs):
+    # Specify the file path (relative or absolute)
+    source_folder = source_docs
+    collection_folder_path = os.path.join(source_folder, collection)
+    folder_path = os.path.join(collection_folder_path, folder_name_to_del)
+    
+    # Check if the file exists before attempting to delete
+    if os.path.exists(folder_path):
+        # If it exists, delete the folder
+        shutil.rmtree(folder_path)
+        print(f"Folder '{folder_path}' has been deleted.")
+        return True
+    else:
+        print(f"Folder '{folder_path}' does not exist.")
+        return False
+
+    
+    
+def texts_splitter(documents):
+    try:
+        # Split documents into chunks
+        # text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        # text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=3000, chunk_overlap=0)
+        texts = text_splitter.split_documents(documents)
+        return texts
+    except Exception as e:
+        error_message = f"An error occurred during text splitting: {str(e)}"
+        raise RuntimeError(error_message)
+        
+        
+def check_qdrant_ingested_files(collection_name, qdrant_client): #to  check the files already ingested or not in the  given qdrant collection
+    try:
+        # qdrant_client = QdrantClient(url=url, api_key=api_key)
+        a = qdrant_client.scroll(
+            collection_name=collection_name,
+            scroll_filter=models.Filter(),
+            limit=100000000000
+        )
+        unique_filenames = {os.path.basename(entry.payload["metadata"]['source']) for entry in a[0]}
+        already_ingested_files = sorted(list(unique_filenames))
+        return already_ingested_files
+    
+    except Exception as e:
+        error_message = f"An error occurred: {str(e)}"
+        raise RuntimeError(error_message)
+        
+        
+        
+def ingesting_file_qdrant_db(collection_name, texts, model_type, url, qdrant_api_key,  embedding_model, cache_folder, openai_api_key):
+    if model_type == "openai":
+        embedding_function = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    elif model_type == "mistral":
+        embedding_function = HuggingFaceEmbeddings(model_name=embedding_model, cache_folder=cache_folder)
+    
+    # Create a vectorstore from documents
+    qdrant = Qdrant.from_documents(
+                        texts,
+                        embedding_function,
+                        url=url,
+                        prefer_grpc=True,
+                        api_key=qdrant_api_key,
+                        collection_name=collection_name,
+                    )
+    return True
+
