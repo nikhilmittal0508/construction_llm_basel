04eccaab55198976fc90011756ffe8528d96058f
Nikhil Mittal <nikhil.mittal0508@gmail.com>
2025-03-10 15:26:46 +0530

    changes

--- sub_sqs.py
+++ sub_sqs.py
@@ -54,57 +54,57 @@ def file_upload_qdrant_sqs(sqs_message):
     model_type = sqs_message['model_type']
     folder_path = sqs_message['folder_path']
 
-    # try:
+    try:
 
-    if filename.lower().endswith((".json")):
-        loader = JSONLoader(file_path, jq_schema='.content')
-        documents = loader.load()
+        if filename.lower().endswith((".json")):
+            loader = JSONLoader(file_path, jq_schema='.content')
+            documents = loader.load()
 
-    elif filename.lower().endswith((".csv")):
-        loader = CSVLoader(file_path)
-        documents = loader.load()
+        elif filename.lower().endswith((".csv")):
+            loader = CSVLoader(file_path)
+            documents = loader.load()
 
-    elif file_path.endswith(".pdf"):
-        loader = PyPDFLoader(file_path)
-        documents = loader.load()
+        elif file_path.endswith(".pdf"):
+            loader = PyPDFLoader(file_path)
+            documents = loader.load()
 
-    elif file_path.endswith('.txt'):
-        loader = TextLoader(file_path)
-        documents = loader.load()
+        elif file_path.endswith('.txt'):
+            loader = TextLoader(file_path)
+            documents = loader.load()
 
-    elif file_path.endswith('.docx'):
-        loader = Docx2txtLoader(file_path)
-        documents = loader.load()
+        elif file_path.endswith('.docx'):
+            loader = Docx2txtLoader(file_path)
+            documents = loader.load()
 
-    else:
-        loader = UnstructuredFileLoader(file_path)
-        documents = loader.load()
+        else:
+            loader = UnstructuredFileLoader(file_path)
+            documents = loader.load()
 
-    # Split documents into chunks
-    texts = texts_splitter(documents)
-    
-    # Ingestion
-    response = ingesting_file_qdrant_db(collection_name, texts, model_type, config["url"], config["qdrant_api_key"],
-					config['embedding_model'], config['cache_folder'], config['open_api_key'])
-    # print("response:", response)
-    
-    if response:
-        return "Ingesting Done"
-    else:
-        return "Ingestion Failed"
+        # Split documents into chunks
+        texts = texts_splitter(documents)
+        
+        # Ingestion
+        response = ingesting_file_qdrant_db(collection_name, texts, model_type, config["url"], config["qdrant_api_key"],
+    					config['embedding_model'], config['cache_folder'], config['open_api_key'])
+        # print("response:", response)
+        
+        if response:
+            return "Ingesting Done"
+        else:
+            return "Ingestion Failed"
 
-    # except Exception as e:
-    # # If the file was saved, attempt to remove it
-    #     if 'file_path' in locals():
-    #         try:
-    #             os.remove(file_path)
-    #         except:
-    #             pass  # If file removal fails, we don't want to mask the original error
+    except Exception as e:
+    # If the file was saved, attempt to remove it
+        if 'file_path' in locals():
+            try:
+                os.remove(file_path)
+            except:
+                pass  # If file removal fails, we don't want to mask the original error
 
-    #     # Log the error (you might want to use a proper logging system)
-    #     print(f"An error occurred: {str(e)}")
-    #     # Return a user-friendly error message
-    #     return f"An error occurred during file processing: {str(e)}"
+        # Log the error (you might want to use a proper logging system)
+        print(f"An error occurred: {str(e)}")
+        # Return a user-friendly error message
+        return f"An error occurred during file processing: {str(e)}"
 
 def folder_upload_qdrant_sqs(sqs_message):
     zip_file_path = sqs_message['zip_file_path']
@@ -112,64 +112,64 @@ def folder_upload_qdrant_sqs(sqs_message):
     collection_name = sqs_message['collection_name']
     model_type = sqs_message['model_type']
 
-    # try:
-    with ZipFile(zip_file_path, 'r') as zObject:
-    	zObject.extractall(path=destination_folder)
+    try:
+        with ZipFile(zip_file_path, 'r') as zObject:
+        	zObject.extractall(path=destination_folder)
 
-    if os.path.exists(zip_file_path):
-    	os.remove(zip_file_path)
-    else:
-    	print("The file does not exist")
+        if os.path.exists(zip_file_path):
+        	os.remove(zip_file_path)
+        else:
+        	print("The file does not exist")
 
-    file_list = []
-    full_folder_path = zip_file_path.split('.')[0]
-    for root, dirs, files in os.walk(full_folder_path):
-    	for file_name in files:
-    		file_path = os.path.join(root, file_name)
-    		if file_path.endswith((".txt", ".pdf", ".docx", ".xlsx", ".pptx", ".csv", ".html", ".json", ".wav", ".mp3", ".eml")):
-    			file_list.append(file_path)
+        file_list = []
+        full_folder_path = zip_file_path.split('.')[0]
+        for root, dirs, files in os.walk(full_folder_path):
+        	for file_name in files:
+        		file_path = os.path.join(root, file_name)
+        		if file_path.endswith((".txt", ".pdf", ".docx", ".xlsx", ".pptx", ".csv", ".html", ".json", ".wav", ".mp3", ".eml")):
+        			file_list.append(file_path)
 
-    print("to_be_ingested:", file_list)
+        print("to_be_ingested:", file_list)
 
-    if len(file_list) > 0:
-    	errors = []
-    	try:
-    		with ProcessPoolExecutor(max_workers=1) as executor:
-    			futures = []
-    			for i, file_path in tqdm.tqdm(enumerate(file_list)):
-    				try:
-    					print("\n new file getting ingested....")
-    					future = executor.submit(ingest_file_process, destination_folder, os.path.normpath(file_path),collection_name, model_type)
-    					futures.append(future)
-    				except Exception as e:
-    					error_message = f"Error submitting task for file {file_path}: {str(e)}"
-    					print(error_message)  # Print the error
-    					errors.append(error_message)  # Add the error to the list
+        if len(file_list) > 0:
+        	errors = []
+        	try:
+        		with ProcessPoolExecutor(max_workers=2) as executor:
+        			futures = []
+        			for i, file_path in tqdm.tqdm(enumerate(file_list)):
+        				try:
+        					print("\n new file getting ingested....")
+        					future = executor.submit(ingest_file_process, destination_folder, os.path.normpath(file_path),collection_name, model_type)
+        					futures.append(future)
+        				except Exception as e:
+        					error_message = f"Error submitting task for file {file_path}: {str(e)}"
+        					print(error_message)  # Print the error
+        					errors.append(error_message)  # Add the error to the list
 
-    			# Wait for all tasks to complete
-    			results = []
-    			for future in futures:
-    				try:
-    					result = future.result()
-    					results.append(result)
-    				except Exception as e:
-    					print(f"Error in task execution: {str(e)}")
+        			# Wait for all tasks to complete
+        			results = []
+        			for future in futures:
+        				try:
+        					result = future.result()
+        					results.append(result)
+        				except Exception as e:
+        					print(f"Error in task execution: {str(e)}")
 
-    	except Exception as e:
-    		print(f"An error occurred in the process pool execution: {str(e)}")
-    	finally:
-    		# Ensure the executor is shut down properly
-    		executor.shutdown(wait=True)
+        	except Exception as e:
+        		print(f"An error occurred in the process pool execution: {str(e)}")
+        	finally:
+        		# Ensure the executor is shut down properly
+        		executor.shutdown(wait=True)
 
-    # After all operations, save the errors to a file
-    with open('error_log.json', 'w') as f:
-    	json.dump(errors, f, indent=4)
+        # After all operations, save the errors to a file
+        with open('error_log.json', 'w') as f:
+        	json.dump(errors, f, indent=4)
 
-    # Open the file in write mode
-    return "Ingestion Complete"
+        # Open the file in write mode
+        return "Ingestion Complete"
 
-    # except Exception as e:
-    # 	return f"Ingestion Incomplete with error: {str(e)}"
+    except Exception as e:
+    	return f"Ingestion Incomplete with error: {str(e)}"
 
 
 def ingest_file_process(folder_path, file_path, collection_name, model_type):
